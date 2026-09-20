1406aafedf9ebf1614eedeb8ff48b253ed98ea4b
poiyomi <work@poiyomi.com>
2026-09-20 13:58:43 -0700

    Share section and subsection IMGUI rendering

--- Editor/EditorStructs/ShaderGroup.cs
+++ Editor/EditorStructs/ShaderGroup.cs
@@ -383,6 +383,92 @@ namespace Thry.ThryEditor
             }
         }
 
+        protected void DrawSection(bool isSubsection)
+        {
+            const int BORDER_WIDTH = 2;
+            const int HEADER_HEIGHT = InspectorTheme.SectionHeight;
+            const int CHECKBOX_OFFSET = 20;
+            if (Options.margin_top > 0)
+            {
+                GUILayoutUtility.GetRect(0, Options.margin_top);
+            }
+
+            ShaderProperty reference = Options.reference_property != null ? MyShaderUI.PropertyDictionary[Options.reference_property] : null;
+            int sectionToggleWidth = SectionEditing.IsEditing(this) ? SlidingToggle.ReservedWidth : 0;
+            bool has_header = string.IsNullOrWhiteSpace(this.Content.text) == false || reference != null || sectionToggleWidth > 0;
+
+            int headerTextX = 18 + sectionToggleWidth;
+            int height = (has_header ? HEADER_HEIGHT : 0) + 4; // 4 for border margin
+
+            // Draw border
+            Rect border = EditorGUILayout.BeginVertical();
+            float rightEdge = border.x + border.width;
+            if (isSubsection) rightEdge -= GUILib.SectionContentRightPadding + 3;
+            border.x = GUILib.GetPropertyX(this.XOffset) - BORDER_WIDTH;
+            border.width = rightEdge - border.x - (isSubsection ? 0 : 1);
+            border = new RectOffset(0, 0, -2, -2).Add(border);
+            InspectorTheme.DrawSection(this, border, IsExpanded, has_header, isSubsection);
+
+            // Draw Reference
+            Rect clickCheckRect = GUILayoutUtility.GetRect(0, height);
+            if (reference != null)
+            {
+                EditorGUI.BeginChangeCheck();
+                Rect referenceRect = new Rect(border.x + CHECKBOX_OFFSET + sectionToggleWidth, border.y + (HEADER_HEIGHT - 18) / 2, 18, 18);
+                using (new SectionEditing.HeaderTintScope(this))
+                    reference.Draw(referenceRect, new GUIContent(), isInHeader: true, useEditorIndent: true);
+                headerTextX = CHECKBOX_OFFSET + 20 + sectionToggleWidth;
+                // Change expand state if reference is toggled
+                if (EditorGUI.EndChangeCheck() && Options.ref_float_toggles_expand)
+                {
+                    IsExpanded = reference.MaterialProperty.GetNumber() == 1;
+                }
+            }
+
+            // Draw Header (GUIContent)
+            Rect top_border = new Rect(border.x, border.y, border.width - 20, HEADER_HEIGHT);
+            if (has_header)
+            {
+                Rect header_rect = new RectOffset(headerTextX, 0, 0, 0).Remove(top_border);
+                using (new SectionEditing.HeaderTintScope(this))
+                    GUI.Label(header_rect, this.Content, isSubsection ? InspectorTheme.Subsection : InspectorTheme.Section);
+            }
+
+            // Draw menu icon
+            if (has_header)
+            {
+                using (new SectionEditing.HeaderTintScope(this, graphics: true))
+                    DrawMenuIcon(border, Event.current);
+            }
+
+            // Toggling + Draw Arrow
+            if (sectionToggleWidth > 0)
+                SectionEditing.DrawHeaderToggle?.Invoke(this, new Rect(border.x + 18, border.y + (HEADER_HEIGHT - 16) / 2, SlidingToggle.Width, 16));
+            FoldoutArrow(top_border, Event.current);
+            if (Event.current.type == EventType.MouseDown && clickCheckRect.Contains(Event.current.mousePosition))
+            {
+                IsExpanded = !IsExpanded;
+                Event.current.Use();
+            }
+
+            // Draw Children
+            if (IsExpanded)
+            {
+                GUILib.SectionContentPadding = isSubsection ? GUILib.SectionContentPadding + 2 : 4;
+                GUILib.SectionContentRightPadding = isSubsection ? GUILib.SectionContentRightPadding + 2 : 2;
+                EditorGUI.BeginDisabledGroup(DoDisableChildren);
+                foreach (ShaderPart part in Children)
+                {
+                    part.Draw();
+                }
+                EditorGUI.EndDisabledGroup();
+                GUILib.SectionContentPadding = isSubsection ? GUILib.SectionContentPadding - 2 : 0;
+                GUILib.SectionContentRightPadding = isSubsection ? GUILib.SectionContentRightPadding - 2 : 0;
+                if (!isSubsection) GUILayoutUtility.GetRect(0, 5);
+            }
+            EditorGUILayout.EndVertical();
+        }
+
         public override void FindUnusedTextures(List<string> unusedList, bool isEnabled)
         {
             if (isEnabled && Options.condition_enable != null)
